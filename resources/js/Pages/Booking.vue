0d502fe634f7dfaf019a6262806089347a547661
Balázs Györkös <balazs.gyorkos@gmail.com>
2023-09-06 22:51:16 +0200

    WIP - Work in progress ;)

---
+++ resources/js/Pages/Booking.vue
@@ -0,0 +1,41 @@
+<script>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+    components: {
+        FullCalendar, // make the <FullCalendar> tag available
+        AuthenticatedLayout,
+        Head
+
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [ dayGridPlugin, interactionPlugin ],
+                initialView: 'dayGridMonth'
+            }
+        }
+    }
+}
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Booking</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <FullCalendar :options="calendarOptions" />
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
