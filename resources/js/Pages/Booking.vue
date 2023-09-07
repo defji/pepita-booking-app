baa372ddb385d3fe4c1c24f72dc844e550a19b76
Balázs Györkös <balazs.gyorkos@gmail.com>
2023-09-07 21:20:10 +0200

    WIP - Work in progress ;)

--- resources/js/Pages/Booking.vue
+++ resources/js/Pages/Booking.vue
@@ -3,6 +3,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/vue3';
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
 export default {
@@ -15,9 +16,17 @@ export default {
     data() {
         return {
             calendarOptions: {
-                plugins: [dayGridPlugin, interactionPlugin],
-                initialView: 'dayGridMonth',
-                dateClick: this.handleDateClick,
+                plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+                initialView: 'timeGridWeek',
+                //dateClick: this.handleDateClick,
+                selectable: true,
+                select: this.handleDateSelect,
+
+                headerToolbar: {
+                    left: 'prev,next',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay' // user can switch between the two
+                },
                 events: [{
                     'title': 'jujj nap',
                     'start': '2023-09-06 23:00:00',
@@ -29,8 +38,8 @@ export default {
         }
     },
     methods: {
-        handleDateClick: function (arg) {
-            alert('date click! ' + arg.dateStr)
+        handleDateSelect(info) {
+            alert(JSON.stringify((info)));
         }
     }
 }
